149d3c7a68d5471ead3d618a1918f5d38ce4fbda
Josías Yah <josiasyah05@gmail.com>
2025-07-07 17:32:33 -0600

    Fixed code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
+// Uso de backticks en vez de comillas simples
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+// Arreglo en el uso de . para seleccionar la clase en vez de un #
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+// Cambio a  funcion asincrona para el uso de await
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  // Variable agregada para recibir la respuesta del fetch y convertirla en string  
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // Agregado ? para asociarlo correctamente con la variable declarada
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
